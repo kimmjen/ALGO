6b7b142d8344c482d75ea0b14c569f80e306c408
kimmjen <wpals814@gmail.com>
2020-11-23 22:11:42 +0900

    step8

--- step8/p_1712/p_1712.java
+++ step8/p_1712/p_1712.java
@@ -1,24 +1,21 @@
+package step8.p_1712;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class p_1712 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        if(C <= B) {
+        for(C <= B) {
             System.out.println("-1");
         } else {
             System.out.println(A/(C-B) + 1);
         }
     }
-    
 }
